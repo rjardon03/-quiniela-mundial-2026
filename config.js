556d69d5303f3d6d8cd849e89d1b7edc65e04514
Rolando Ardon <112026395+rjardon03@users.noreply.github.com>
2026-06-17 17:30:02 -0600

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // MODO GRUPO: crear un proyecto en Supabase y completar estos valores.
 window.QUINIELA_CONFIG = {
   appName: 'Quiniela Mundialista 2026',
-  supabaseUrl: '',
-  supabaseAnonKey: '',
+  supabaseUrl: 'https://klbosgthllirmhkuzlkl.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtsYm9zZ3RobGxpcm1oa3V6bGtsIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4MTcyNjExMCwiZXhwIjoyMDk3MzAyMTEwfQ.c8jz1pEXHhTe9sCX9DfTDfz9O8VMsUf9DdGVXzlIwYo',
   lockPredictionsAtKickoff: true
 };
