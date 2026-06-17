ab8af2d9562d4f0f147a829b345fb4d8a22c61a2
Rolando Ardon <112026395+rjardon03@users.noreply.github.com>
2026-06-17 17:42:47 -0600

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,6 @@
 window.QUINIELA_CONFIG = {
   appName: 'Quiniela Mundialista 2026',
   supabaseUrl: 'https://klbosgthllirmhkuzlkl.supabase.co',
-  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtsYm9zZ3RobGxpcm1oa3V6bGtsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE3MjYxMTAsImV4cCI6MjA5NzMwMjExMH0.Y4PH-TVo2MlrWD1IvaR940zeTav44M-v31JrFD1vT1I',
+  supabaseAnonKey: 'sb_secret_t-0518_3i4xmM-_c0NkqEQ__9-pfCFK',
   lockPredictionsAtKickoff: true
 };
